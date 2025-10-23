df492a725df080e46a3036a71ddb95159c775a96
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-10-24 01:23:46 +0600

    Login and Register ui created

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,38 @@
 // It's Register page.
+
 const Register = () => {
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log('Email: ', email, 'Password: ', password)
+
+    }
     return (
-        <div>
-            <h2>Register Page.</h2>
+        <div
+            className='bg-green-100  rounded-xl border-black-700 p-3 m-3 w-2/4 mx-auto mt-[250px]'>
+            <h2
+                className="text-2xl text-center font-extrabold m-4"
+            >
+                Please Register</h2>
+
+            <form onSubmit={handleRegister} >
+                <div className=" w-[500px] mx-auto">
+                    <input type="name" placeholder='enter name' name="name" id="" required className='border-2 w-full border-black rounded-lg mb-2 p-2 mx-auto' />
+                    <br />
+                    <input type="email" placeholder='enter email' name="email" id="" required className='border-2 w-full border-black rounded-lg mb-2 p-2 mx-auto' />
+                    <br />
+                    <input type="password" placeholder='enter password' name="password" required id="" className='border-2 rounded-lg border-black mb-2 p-2 w-full' />
+                    <br />
+                    <br />
+                    
+                </div>
+                <input type="submit" value="Register" className='border-2 bg-green-400 text-white font-bold border-black p-2 rounded-lg w-full mb-4' />
+            </form>
         </div>
+
     );
 };
 
