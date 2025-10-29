24857bd22610a40ad8be8e9d852fea239c3e3ab3
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-10-29 18:38:20 +0600

    Updated ui

--- src/components/Home/ Vehicle_card.jsx
+++ src/components/Home/ Vehicle_card.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
 
 const Vehicle_card = ({ vehicle }) => {
-    const { _id, category, vehicleTitle, price, imageURL } = vehicle;
+    const { _id, category, title, price, imageURL } = vehicle;
 
     return (
         <Link to={`/vehicle/${_id}`}>
-            <div className="border border-green-500 p-3 bg-green-50 rounded-lg hover:shadow-md hover:scale-[1.02] transition-transform">
+            <div className="p-6 bg-green-50 rounded-lg hover:shadow-md hover:scale-[1.02] transition-transform">
                 <img
                     src={imageURL}
-                    alt={vehicleTitle}
+                    alt={title}
                     className="w-full h-48 object-cover rounded-md"
                 />
                 <p className="text-sm text-gray-500 mt-2">{category}</p>
-                <h3 className="text-lg font-semibold text-gray-800">{vehicleTitle}</h3>
+                <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
                 <p className="text-green-600 font-bold">৳ {price} / day</p>
             </div>
         </Link>
