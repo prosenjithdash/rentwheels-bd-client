34cf83b6cfa853300bbd88410d6e28b7d5a1aad0
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-01-08 09:19:45 +0600

    Admin Statistic page add

--- src/pages/Dashboard/Common/Statistics.jsx
+++ src/pages/Dashboard/Common/Statistics.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
+import useRole from '../../../hooks/useRole';
+import AdminStatistics from '../Admin/AdminStatistics';
 
 const Statistics = () => {
-    return (
-        <div>
-            <h1 className='text-4xl font-extrabold text-green-500 min-h-screen text-center '>Welcome to Dashboard: Statistics Page</h1>
-          
-        </div>
-    );
+    const [role, isLoading] = useRole()
+    return <>{ role === "admin" && <AdminStatistics/>}</>
 };
 
 export default Statistics;
---
+++ src/pages/Dashboard/Renter/RenterStatistics.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RenterStatistics = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RenterStatistics;
