0695d1c2ab72135da5612478f279778868890e4d
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-01-09 16:51:56 +0600

    HostStatistics ui set done

--- src/pages/Dashboard/Common/Statistics.jsx
+++ src/pages/Dashboard/Common/Statistics.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import useRole from '../../../hooks/useRole';
 import AdminStatistics from '../Admin/AdminStatistics';
+import HostStatistics from '../Host/HostStatistics';
+import RenterStatistics from '../Renter/RenterStatistics';
 
 const Statistics = () => {
+
     const [role, isLoading] = useRole()
-    return <>{ role === "admin" && <AdminStatistics/>}</>
+
+    return <>
+        
+        {
+            role === "admin" && <AdminStatistics />
+        }
+        
+        {
+            role === "host" && <HostStatistics />
+        }
+
+        {
+            role === "render" && <RenterStatistics />
+        }
+
+    </>
+    
 };
 
 export default Statistics;
