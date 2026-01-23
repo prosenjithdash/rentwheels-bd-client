656b44fb937cf465d695db829e8ff30e5cdba82d
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-01-23 17:23:55 +0600

    modal enclude with formating

--- src/components/Dashboard/Modal/UpdateVehicleModal.jsx
+++ src/components/Dashboard/Modal/UpdateVehicleModal.jsx
@@ -30,6 +30,7 @@ const UpdateVehicleModal = ({ setIsUpdateModalOpen, isOpen, vehicle, refetch }) 
         console.log(item.selection.startDate)
         setDates(item.selection)
         
+
         setVehicleData({
             ...vehicleData,
             to: item.selection.endDate,
