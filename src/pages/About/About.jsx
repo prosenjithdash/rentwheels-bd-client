4859b4c790d62aa49bff05441fb8fb8937e94863
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-02-09 23:55:40 +0600

    About page working

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,7 @@
 const About = () => {
     return (
         <div>
+            {/* We will create component on Component folder then linkup here... */}
             <h2>About page.</h2>
         </div>
     );
