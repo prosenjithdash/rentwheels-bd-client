4b16771b42a58b7205241b03263b8a56bee567cc
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-02-11 14:08:25 +0600

    About count part properly ui done

--- src/components/About/AboutBanner.jsx
+++ src/components/About/AboutBanner.jsx
@@ -2,22 +2,22 @@
 const AboutBanner = () => {
     return (
     
-        <div className="relative h-[350px] md:h-[500px] lg:h-[700px] bg-[url('/assets/AboutImages/b.webp')] bg-cover bg-center">
+        <div className="relative h-[350px] md:h-[500px] lg:h-[450px] bg-[url('/assets/AboutImages/b.webp')] bg-cover bg-center">
 
             {/* Blue overlay */}
             <div className="absolute inset-0 bg-gradient-to-b 
-            from-[#0F172A]/90 
-            via-[#0B1220]/80 
-            to-[#0F172A]/90">
+            from-[#0F172A]/100 
+            via-[#0B1220]/85 
+            to-[#0F172A]/100">
             </div>
 
 
             {/* Content */}
-            <div className="relative z-10 flex items-center justify-center h-full text-white  border border-green-700">
+            <div className="relative z-10 flex items-center justify-center h-full text-white">
                 <div className="  max-w-[750px] mx-auto space-y-4 md:space-y-5 text-center">
-                    <h2 className="inline-block mx-auto font-extrabold text-orange-600 rounded-full text-xs sm:text-sm md:text-base py-1 px-4 bg-gray-800 border border-orange-600">EST. 2020. DHAKA, BANGLADESH</h2>
+                    <h2 className="inline-block mx-auto font-extrabold text-orange-500 rounded-full text-xs sm:text-sm md:text-base py-1 px-4 bg-gray-800 border border-orange-500">EST. 2020. DHAKA, BANGLADESH</h2>
                     <h1 className="font-bold text-3xl sm:text-4xl md:text-6xl lg:text-7xl ">
-                        Driving Bangladesh <br /> <span className="text-orange-600 ">Forward</span>
+                        Driving Bangladesh <br /> <span className="text-orange-500 ">Forward</span>
                     </h1>
                     <p className="text-sm md:text-xl lg:text-2xl  text-gray-300">
                         From a small garage with 10 bikes to the nation's most trusted vehicle rental platform. This is our story.
--- src/components/About/AboutCount.jsx
+++ src/components/About/AboutCount.jsx
@@ -1,13 +1,16 @@
-import { FiCalendar } from "react-icons/fi";
+import { FiCalendar, FiUsers } from "react-icons/fi";
+import { IoCarSportOutline, IoLocationOutline } from "react-icons/io5";
+
 
 const AboutCount = () => {
     return (
-        <div className="bg-gradient-to-b from-[#0F172A] via-[#0B1220] to-[#0F172A] h-[200px] md:h-[300px] lg:h-[350px] border border-red-700">
+        <div className="bg-gradient-to-b from-[#0F172A] via-[#0B1220] to-[#0F172A] h-[200px] md:h-[300px] lg:h-[350px] border-t border-t-gray-800 border-b border-b-gray-800  border-2 border-yellow-600 flex items-center ">
             {/* Content */}
             {/* Main div */}
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 w-full gap-6">
+
                 {/* Years of service */}
-                <div className="border border-red-700 space-y-3 md:space-y-4 text-center">
+                <div className="space-y-4 md:space-y-8 text-center">
                     {/* Icon */}
                     <div className="flex justify-center">
                         <div className="flex items-center justify-center
@@ -22,24 +25,89 @@ const AboutCount = () => {
                         </div>
                     </div>
                     
-                    <div className="text-center">
+                    <div className="text-center space-y-1 md:space-y-1">
                         {/* number of years */}
-                        <h2 className="text-md md:text-lg lg:text-4xl font-extrabold text-white">5+</h2>
+                        <h2 className="text-md md:text-lg lg:text-4xl font-bold text-white">5+</h2>
                         {/* Sentence */}
-                        <p className="text-gray-400">YEARS OF SERVICE</p>
+                        <p className="text-gray-400 ">YEARS OF SERVICE</p>
                     </div>
 
                 </div>
-                {/* Vehicle in fleet */}
-                <div className="border border-red-700">
 
-                </div >
-                {/* Happy Customers */}
-                <div className="border border-red-700">
+                {/* Vehicle in fleet */}
+                <div className="space-y-4 md:space-y-8 text-center">
+                    {/* Icon */}
+                    <div className="flex justify-center">
+                        <div className="flex items-center justify-center
+                        w-12 h-12        
+                        sm:w-14 sm:h-14 
+                        md:w-16 md:h-16 
+                        lg:w-12 lg:h-12 
+                        bg-yellow-950 rounded-full
+                        ">
+                            <IoCarSportOutline className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-orange-600" />
+
+                        </div>
+                    </div>
+
+                    <div className="text-center space-y-1 md:space-y-1">
+                        {/* number of years */}
+                        <h2 className="text-md md:text-lg lg:text-4xl font-bold text-white">500+</h2>
+                        {/* Sentence */}
+                        <p className="text-gray-400">VEHICLE IN FLEET</p>
+                    </div>
 
                 </div>
+
+                {/* Happy Customers */}
+                <div className=" space-y-4 md:space-y-8 text-center">
+                    {/* Icon */}
+                    <div className="flex justify-center">
+                        <div className="flex items-center justify-center
+                        w-12 h-12        
+                        sm:w-14 sm:h-14 
+                        md:w-16 md:h-16 
+                        lg:w-12 lg:h-12 
+                        bg-yellow-950 rounded-full
+                        ">
+
+                            <FiUsers className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-orange-600" />
+
+                        </div>
+                    </div>
+
+                    <div className="text-center space-y-1 md:space-y-1">
+                        {/* number of years */}
+                        <h2 className="text-md md:text-lg lg:text-4xl font-bold text-white">1O,000+</h2>
+                        {/* Sentence */}
+                        <p className="text-gray-400">HAPPY CUSTOMERS</p>
+                    </div>
+
+                </div>
+
                 {/*City covered */}
-                <div className="border border-red-700">
+                <div className=" space-y-4 md:space-y-8 text-center">
+                    {/* Icon */}
+                    <div className="flex justify-center">
+                        <div className="flex items-center justify-center
+                        w-12 h-12        
+                        sm:w-14 sm:h-14 
+                        md:w-16 md:h-16 
+                        lg:w-12 lg:h-12 
+                        bg-yellow-950 rounded-full
+                        ">
+
+                        <IoLocationOutline className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-orange-600" />
+
+                        </div>
+                    </div>
+
+                    <div className="text-center space-y-1 md:space-y-1">
+                        {/* number of years */}
+                        <h2 className="text-md md:text-lg lg:text-4xl font-bold text-white">50+</h2>
+                        {/* Sentence */}
+                        <p className="text-gray-400">CITIES COVERED</p>
+                    </div>
 
                 </div>
 
