a7fbbed05d55fa54dddf7436bef3d0dbd33efc3b
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-02-13 14:53:03 +0600

    Responsive of Mission part

--- src/components/About/Mission_Vision.jsx
+++ src/components/About/Mission_Vision.jsx
@@ -3,17 +3,17 @@ import { IoFlagOutline } from "react-icons/io5";
 
 const Mission_Vision = () => {
     return (
-        <div className="bg-gradient-to-b from-[#0F172A] via-[#0B1220] to-[#0F172A] py-8 sm:py-10  md:py-16 lg:py-24">
+        <div className="bg-gradient-to-b from-[#0F172A] via-[#0B1220] to-[#0F172A] py-8 sm:py-10 p-4  md:py-16 lg:py-24">
             
             {/* Main Mission & Vision part */}
-            <div className="flex justify-between gap-6 max-w-7xl mx-auto ">
+            <div className="lg:flex justify-between gap-6 max-w-7xl mx-auto ">
 
                 {/* Mission card */}
                 <div className="bg-gradient-to-b from-orange-500/15  to-orange-500/2  space-y-4 p-12  rounded-2xl border border-gray-700">
                                         {/* Icon */}
                     <div className="">
                         <div className="flex items-center justify-center
-                        w-12 h-12
+                        w-14 h-14
                         sm:w-14 sm:h-14
                         md:w-16 md:h-16
                         lg:w-16 lg:h-16
@@ -24,8 +24,8 @@ const Mission_Vision = () => {
                         </div>
                     </div>
                                        
-                    <h2 className="text-gray-200 text-4xl font-bold">Our Mission</h2>
-                    <p className="text-gray-400 text-lg">
+                    <h2 className="text-gray-200 text-3xl lg:text-4xl font-bold">Our Mission</h2>
+                    <p className="text-gray-400 text-md lg:text-lg">
                         To provide accessible, Reliable, And premium transportation solutions to everyone in Bangladesh, Making every journey memorable and hassle-free through technology and superior service.
                     </p>
                 
