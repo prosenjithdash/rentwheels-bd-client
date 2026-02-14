bba7b64df326d7fae33bad280a1e341cf7741155
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-02-14 18:04:29 +0600

    Team title part

--- src/components/About/Team.jsx
+++ src/components/About/Team.jsx
@@ -1,8 +1,18 @@
 
 const Team = () => {
     return (
-        <div>
-            <h2>RentWheels_BD Team.</h2>
+        <div className="bg-gradient-to-b from-[#0B1220] to-[#020617] py-8 sm:py-10  md:py-16 lg:py-28  h-24">
+            <div className="max-w-7xl mx-auto ">
+                {/* Team title part */}
+                <div className="text-center space-y-3">
+                    <h1 className="text-3xl lg:text-5xl text-gray-100 font-bold">
+                        The People Behind the Wheels
+                    </h1>
+                    <p className="text-gray-400 text-lg">
+                        Meet the dedicated team working tirelessly to ensure your journey is smooth and safe.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,7 @@ import AboutBanner from "../../components/About/AboutBanner";
 import AboutCount from "../../components/About/AboutCount";
 import Mission_Vision from "../../components/About/Mission_Vision";
 import OurStory from "../../components/About/OurStory";
+import Team from "../../components/About/Team";
 import WhatDrivesUs from "../../components/About/WhatDrivesUs";
 
 const About = () => {
@@ -12,7 +13,8 @@ const About = () => {
             <AboutCount />
             <OurStory />
             <WhatDrivesUs />
-            <Mission_Vision/>
+            <Mission_Vision />
+            <Team/>
         </div>
     );
 };
